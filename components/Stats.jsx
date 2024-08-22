30dbe64bff8c9c4b7f2edeba763539e9174825d0
Yueh Chun Wu <yuehcw@uci.edu>
2024-08-21 21:43:42 -0700

    Updating Stats Mobile Width

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -25,7 +25,7 @@ const Stats = () => {
   return (
     <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
       <div className="container mx-auto">
-        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+        <div className="flex flex-wrap gap-6 max-w-[120vw] mx-auto xl:max-w-none">
           {stats.map((item, index) => {
             return (
               <div
