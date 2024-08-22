a32863ac76569621ac1c3a11eb454e85fbc67c63
Yueh Chun Wu <yuehcw@uci.edu>
2024-08-21 18:28:43 -0700

    added

---
+++ components/Header.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+// components
+import Nav from "./Nav";
+import MobileNav from './MobileNav';
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Kevin<span className="text-accent">.</span>
+          </h1>
+        </Link>
+        {/* desktop nav & hire me button*/}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+
+        {/* mobile nav */}
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/Social.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { FaGithub, FaLinkedinIn, FaTwitter, FaYoutube } from "react-icons/fa";
+
+const soicals = [
+  { icon: <FaGithub />, path: "" },
+  { icon: <FaLinkedinIn />, path: "" },
+  { icon: <FaYoutube />, path: "" },
+  { icon: <FaTwitter />, path: "" },
+];
+
+const Social = ({ containerStyles, iconStyles }) => {
+  return (
+    <div className={containerStyles}>
+      {soicals.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Social;
