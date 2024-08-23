ef33884a00bfa51af1f44125badd8aee34a0da6d
Yueh Chun Wu <yuehcw@uci.edu>
2024-08-23 15:30:27 -0700

    Fixing typo

--- app/page.jsx
+++ app/page.jsx
@@ -25,7 +25,7 @@ const Home = () => {
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button variant="outline" size="lg" className="uppercase">
                 <a
-                  href="https://drive.google.com/file/d/1W_B3ym8QgvFjr6qevWDjqjyhkKUgtBB4/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1a8PkZWc9ohD6SpzqPS0QZSotyLDsKQC_/view?usp=sharing"
                   download
                   className="flex items-center gap-2"
                 >
