a98eab43dbf7e197f733bbdab5e7061cf36817d6
Yueh Chun Wu <yuehcw@uci.edu>
2024-11-14 23:00:57 -0800

    Update Resume

--- app/page.jsx
+++ app/page.jsx
@@ -25,7 +25,7 @@ const Home = () => {
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button variant="outline" size="lg" className="uppercase">
                 <a
-                  href="https://drive.google.com/file/d/1K1Kf4YkkmaT9VPNGVsseBpafQqBVO8A1/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1nBocvbMTP2Dv9U8v77J04skrHAeEsoLS/view?usp=sharing"
                   download
                   className="flex items-center gap-2"
                 >
